4a252c3a144607ea78a8bddad2aa2dee1a0acf3e
Ahmed Aldesoky <ahmedaldesoky134@gmail.com>
2023-04-10 06:51:08 +0200

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./*.{html,ts}"],
+//   darkMode: ['class', '[data-mode="dark"]'],
+//   theme: {
+//     screens: {
+//       xs: '480px',
+//       sm: '768px',
+//       md: '976px',
+//       lg: '1450px',
+//       xl: '1480',
+//       xxl: '1680px'
+//     },
+//     fontSize: {
+//       sm: '0.8rem',
+//       base: '1rem',
+//       xl: '1.25rem',
+//       '2xl': '1.563rem',
+//       '3xl': '1.953rem',
+//       '4xl': '2.441rem',
+//       '5xl': '3.052rem',
+//     },
+//     colors: {
+//       'white': '#fff',
+//       'main': '#1E999D',
+//       'main-dark': '#0D6C71',
+//       'main-light': '#23C1C6',
+//       'main-meduim': '#229EA2',
+//       'main-extra-dark': '#0F4D50',
+//       'secondary': '#E9973E',
+//       'red': '#DE382C',
+//       'dark': '#383838',
+//       'gray': '#F2F8F8',
+//       'gray-dark': '#707070',
+//       'gray-light': '#d3dce6',
+//       'google': '#EE433D',
+//       'facebook': '#3F66AF',
+//       'apple': '#06324C',
+//       'secondaryopc': '#DF6450',
+//       'error': '#FF6060',
+//       "transparent": 'transparent',
+//       "main-Light-2": '#BDEAEB',
+//       "blue": '#4977F4',
+//       "light-geren": '#F1F8F8',
+//       "light-geren-2": '#F8F8F8',
+//       "light-geren-3": "#67C949"
+//     },
+//     extend: {},
+//   },
+//   plugins: [
+//     require("@tailwindcss/forms"),
+//     require('tw-elements/dist/plugin')
+//   ],
+// }
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
